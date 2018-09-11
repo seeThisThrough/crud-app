b9cdf16b78b5cf70f2fe1197dd8c9e852ef7b842
seeThisThrough <geedyne.li@gmail.com>
2018-09-11 17:36:47 -0500

    still running into problems with delete/edit but one step closer....

--- app.js
+++ app.js
@@ -6,11 +6,11 @@
 var database = firebase.database();
 
 var blogs = [];
-//submit
-//document.getElementById("text-body").onclick = function
 
 var post = firebase.database().ref('users/');
+//console.log(post.key)
 
+//database method which fetches data on update
 post.on('value', function(snapshot) {
   //console.log(snapshot.val());
   removePosts()
@@ -23,10 +23,6 @@ var title = document.getElementById('post-title');
 
 var body = document.getElementById('post-body');
 
-//create userId
-
-
-
 //writeUserData
 function writeData() {
   firebase.database().ref('users/').push({
@@ -37,54 +33,80 @@ function writeData() {
 
 //readUserData
 function blogPost(post) {
-
-
- var placeholder = document.getElementById('placeholder');
+  var placeholder = document.getElementById('placeholder');
  
- var newTitle = document.createElement('h2');
+  var newTitle = document.createElement('h2');
 
    newTitle.appendChild(document.createTextNode(post.title));
-
- var newBody = document.createElement('p');
+ 
+  var newBody = document.createElement('p');
 
    newBody.append(document.createTextNode(post.body));
  
- var editBtn = document.createElement('button');
+  var editBtn = document.createElement('button');
 
- var eText = document.createTextNode('edit');
+  var eText = document.createTextNode('edit');
 
- editBtn.appendChild(eText);
- editBtn.setAttribute('id', 'edit-button');
- editBtn.addEventListener("click", function(){console.log("this is the edit button for: ", post.title)});
+  editBtn.appendChild(eText);
+  editBtn.setAttribute('id', 'edit-button');
+  editBtn.addEventListener("click", function(){editPost(post)});
 
+  var deleteBtn = document.createElement('button');
 
- var deleteBtn = document.createElement('button');
+  var dText = document.createTextNode('delete');
 
- var dText = document.createTextNode('delete');
+  deleteBtn.appendChild(dText);
+  deleteBtn.setAttribute('id', 'delete-button');
+  deleteBtn.addEventListener("click", function(){deletePost(post)})// not working
 
- deleteBtn.appendChild(dText);
- deleteBtn.setAttribute('id', 'delete-button');
- deleteBtn.addEventListener("click", function(){console.log("this is the delete button for:", post.title)})
-
-   placeholder.append(newTitle);
-   placeholder.append(newBody);
-   placeholder.append(editBtn)
-   placeholder.append(deleteBtn) 
+  placeholder.append(newTitle);
+  placeholder.append(newBody);
+  placeholder.append(editBtn)
+  placeholder.append(deleteBtn) 
 }
-
-
 //helper function of readUserData
 function removePosts() {
   var myNode = document.getElementById("placeholder");
   while (myNode.firstChild) {
     myNode.removeChild(myNode.firstChild)
   }
+
 }
 
 
 //Delete Posts
-function deletePost() {
+function deletePost(post) {
+  //firebase.database().ref('users/').update(title, body);
+  console.log(post)
+  console.log("delete function press successful on ", post.title, "and body ", post.body)
+
 }
 
 
+function editPost(post) {
+  console.log(post)
+  console.log("edit function press successful on ", post.title, "and body ", post.body)
+}
+
+// function writeNewPost(uid, username, picture, title, body) {
+//   // A post entry.
+//   var postData = {
+//     author: username,
+//     uid: uid,
+//     body: body,
+//     title: title,
+//     starCount: 0,
+//     authorPic: picture
+//   };
+
+//   // Get a key for a new Post.
+//   var newPostKey = firebase.database().ref().child('posts').push().key;
+
+//   // Write the new post's data simultaneously in the posts list and the user's post list.
+//   var updates = {};
+//   updates['/posts/' + newPostKey] = postData;
+//   updates['/user-posts/' + uid + '/' + newPostKey] = postData;
+
+//   return firebase.database().ref().update(updates);
+// }
 
